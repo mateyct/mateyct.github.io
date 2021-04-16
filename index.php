74f7be7ba96320e99163b64fdaa5114228b3b553
mateyct <61563927+mateyct@users.noreply.github.com>
2021-04-16 10:55:52 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>My GitHub Page</title>
+  </head>
+  <body>
+    <h1>Welcome to my GitHub page</h1>
+  </body>
+</html>
